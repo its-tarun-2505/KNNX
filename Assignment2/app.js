2a375b00c7755c7f53ddae356d485bffdfab148d
Tarun Agarwal <its.tarun.2505@gmail.com>
2025-11-20 16:47:03 +0530

    change js to kn

--- Assignment2/app.js
+++
@@ -1,8 +0,0 @@
-import questions from './data/questions.js';
-import QuizMaster from './game/QuizMaster.js';
-
-// Creating a new QuizMaster and giving it our questions
-const quiz = new QuizMaster(questions);
-
-// Starting the quiz experience
-quiz.start();
